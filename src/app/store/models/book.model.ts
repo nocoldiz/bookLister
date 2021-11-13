1d1eb5499da460417071796b1e743aefc45587b3
Paolo Pirruccio <paolo.pirruccio@ditechspa.it>
2021-11-13 22:50:10 +0100

    added book thumbnail

--- src/app/store/models/book.model.ts
+++ src/app/store/models/book.model.ts
@@ -1,7 +1,22 @@
+interface ImageLinks {
+    smallThumbnail: string;
+    thumbnail: string;
+}
+
 export interface Book {
     id: string;
     volumeInfo: {
         title: string;
+        subtitle: string;
         authors: Array<string>;
+        publisher: string;
+        publishedDate: any;
+        description: string;
+        pageCount: number;
+        categories: string[];
+        language: string;
+        previewLink: string;
+        infoLink: string;
+        imageLinks: ImageLinks;
     };
 }
